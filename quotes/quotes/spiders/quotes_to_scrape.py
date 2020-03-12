249fcc643fa7feef2d6ca6f5b3e9108edbfe02c6
Gustavo Barros Rocha <gus.rochab@gmail.com>
2020-03-12 17:37:05 -0300

    Files added to run quotes spider

--- quotes/quotes/spiders/quotes_to_scrape.py
+++ quotes/quotes/spiders/quotes_to_scrape.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import scrapy
 from scrapy_splash import SplashRequest
-
+import cv2
 
 class QuotesToScrapeSpider(scrapy.Spider):
     name = 'quotes_to_scrape'
@@ -12,18 +12,33 @@ class QuotesToScrapeSpider(scrapy.Spider):
             url = args.url
             assert(splash:go(url))
             assert(splash:wait(1))
-            return splash:html()
+            return splash:png()
         end  
     '''
 
     def start_requests(self):
-        yield SplashRequest(url='http://quotes.toscrape.com', callback=self.parse, endpoint='execute',
-                            args={'lua_source': self.script})
+        s = SplashRequest(url='http://quotes.toscrape.com', callback=self.parse, endpoint='execute',
+                            args={'lua_source': self.script}, dont_process_response=False)
+        print('|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||\n')
+        print(type(s))
+        print('\n')
+        print('|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||\n')
+        yield s
 
     def parse(self, response):
+        print('|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||\n')
+        print(type(response))
+        print('\n\n\n')
+        print('RESPONSE.BODY\n')
+        print(response.body)
+        print('\n\n\n')
+        print('|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||\n')
+        yield response
+
+        '''
         for quote in response.xpath("//div[@class='quote']"):
             yield {
                 'quote': quote.xpath(".//span[@class='text']/text()").get(),
                 'author': quote.xpath(".//span/small/text()").get()
             }
-
+        '''
